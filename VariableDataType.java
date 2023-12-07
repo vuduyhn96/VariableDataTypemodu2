cef9ee015e68cdddfcfca2c0b0952802a04edd1a
vuduyhn96 <vuanhduyhn1996@gmail.com>
2023-12-07 14:23:27 +0700

    VariableDataType

---
+++ VariableDataType.java
@@ -0,0 +1,23 @@
+public class VariableDataType {
+    public static void main(String[] args) {
+        int f = 10;
+                byte i = 100;
+                short a = 1000;
+                long b = 112l;
+                float c = 20.4f;
+                double d = 20.4;
+                boolean h = true;
+                char q = 'q';
+                String s = "hello";
+        System.out.println("i = " + i);
+        System.out.println("a = " + a);
+        System.out.println("b = " + b);
+        System.out.println("c = " + c);
+        System.out.println("d = " + d);
+        System.out.println("h = " + h);
+        System.out.println("q = " + q);
+        System.out.println("s = " + s);
+        System.out.println("f = " + f);
+
+    }
+}
